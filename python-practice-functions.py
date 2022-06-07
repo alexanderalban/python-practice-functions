dca07358d95f7175fa954684392ca06ed5c61550
alexanderalban <albanalexander@gmail.com>
2022-06-07 13:37:24 -0500

    added multiclass object (Superman), works!

--- python-practice-functions.py
+++ python-practice-functions.py
@@ -21,11 +21,12 @@ class morphin_grid_user(earth_defender):
 
 
 class ranger(morphin_grid_user):
-    def __init__(self, name, color, zord, weapon):
+    def __init__(self, name, color, zord, weapon, city):
         self.name = name
         self.color = color
         self.zord = zord
         self.weapon = weapon
+        self.city = city
 
     def morph(self):
         print(self.zord + "!")
@@ -34,29 +35,41 @@ class ranger(morphin_grid_user):
         print("Go Go Power Rangers!")
 
 
-jason = ranger("Jason Lee Scott", "Red", "Tyrannosaurus", "Power Sword")
-kimberly = ranger('Kimberly Hart', "Pink", "Pterodactyl", "Power Bow")
-trini = ranger("Trini Kwan", "Yellow", "Sabretooth Tiger", "Power Daggers")
-zack = ranger("Zack Taylor", "Black", "Mastodon", "Power Axe")
-billy = ranger("Billy Cranston", "Blue", "Triceratops", "Power Lance")
-tommy = ranger("Tommy Oliver", "Green", 'Dragonzord', 'Dragon Dagger')
+jason = ranger("Jason Lee Scott", "Red", "Tyrannosaurus", "Power Sword", "Angel Grove")
+kimberly = ranger('Kimberly Hart', "Pink", "Pterodactyl", "Power Bow", "Angel Grove")
+trini = ranger("Trini Kwan", "Yellow", "Sabretooth Tiger", "Power Daggers", "Angel Grove")
+zack = ranger("Zack Taylor", "Black", "Mastodon", "Power Axe", "Angel Grove")
+billy = ranger("Billy Cranston", "Blue", "Triceratops", "Power Lance", "Angel Grove")
+tommy = ranger("Tommy Oliver", "Green", 'Dragonzord', 'Dragon Dagger', "Angel Grove")
 
 # DC classes
 
 
 class jla_member(earth_defender):
-    def __init__(self, name, alter_ego, powers):
+    def __init__(self, name, alter_ego, city, powers):
         self.name = name
         self.alter_ego = alter_ego
+        self.city = city
         self.powers = powers
+
+class kryptonian:
+    powers = "flight, super speed, x-ray vision, heat vision, super strength"
+    planet = "Krypton"
+
+class supermen(jla_member, kryptonian):
+    pass
+
+batman = jla_member("Batman", "Bruce Wayne", "Gotham City", "World's Greatest Detective, intense physical training,, scientist, billionaire")
+superman = supermen("Superman", "Clark Kent", "Metropolis", kryptonian.powers)
 
 # Marvel Classes
 
 
 class avenger(earth_defender):
-    def __init__(self, name, alter_ego, powers):
+    def __init__(self, name, alter_ego, city, powers):
         self.name = name
         self.alter_ego = alter_ego
+        self.city = city
         self.powers = powers
 
 
@@ -64,4 +77,5 @@ class avenger(earth_defender):
 
 print(kimberly.name)
 tommy.morph()
-print(billy.alignment)
+print(batman.powers)
+print(superman.powers)
