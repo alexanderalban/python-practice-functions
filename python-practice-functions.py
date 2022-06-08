3a08139ff262ae902fa0305f5ee393de9b79624b
alexanderalban <albanalexander@gmail.com>
2022-06-08 10:24:55 -0500

    some formatting changes, no true code changes

--- python-practice-functions.py
+++ python-practice-functions.py
@@ -35,12 +35,17 @@ class ranger(morphin_grid_user):
         print("Go Go Power Rangers!")
 
 
-jason = ranger("Jason Lee Scott", "Red", "Tyrannosaurus", "Power Sword", "Angel Grove")
-kimberly = ranger('Kimberly Hart', "Pink", "Pterodactyl", "Power Bow", "Angel Grove")
-trini = ranger("Trini Kwan", "Yellow", "Sabretooth Tiger", "Power Daggers", "Angel Grove")
+jason = ranger("Jason Lee Scott", "Red", "Tyrannosaurus",
+               "Power Sword", "Angel Grove")
+kimberly = ranger('Kimberly Hart', "Pink", "Pterodactyl",
+                  "Power Bow", "Angel Grove")
+trini = ranger("Trini Kwan", "Yellow", "Sabretooth Tiger",
+               "Power Daggers", "Angel Grove")
 zack = ranger("Zack Taylor", "Black", "Mastodon", "Power Axe", "Angel Grove")
-billy = ranger("Billy Cranston", "Blue", "Triceratops", "Power Lance", "Angel Grove")
-tommy = ranger("Tommy Oliver", "Green", 'Dragonzord', 'Dragon Dagger', "Angel Grove")
+billy = ranger("Billy Cranston", "Blue", "Triceratops",
+               "Power Lance", "Angel Grove")
+tommy = ranger("Tommy Oliver", "Green", 'Dragonzord',
+               'Dragon Dagger', "Angel Grove")
 
 # DC classes
 
@@ -52,14 +57,18 @@ class jla_member(earth_defender):
         self.city = city
         self.powers = powers
 
+
 class kryptonian:
     powers = "flight, super speed, x-ray vision, heat vision, super strength"
     planet = "Krypton"
 
+
 class supermen(jla_member, kryptonian):
     pass
 
-batman = jla_member("Batman", "Bruce Wayne", "Gotham City", "World's Greatest Detective, intense physical training,, scientist, billionaire")
+
+batman = jla_member("Batman", "Bruce Wayne", "Gotham City",
+                    "World's Greatest Detective, intense physical training,, scientist, billionaire")
 superman = supermen("Superman", "Clark Kent", "Metropolis", kryptonian.powers)
 
 # Marvel Classes
